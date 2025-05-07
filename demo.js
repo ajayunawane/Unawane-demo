a8017773fd852177e75c61b541a19ee56e42794d
ajayunawane <ajayunawane9921@gmail.com>
2025-05-07 18:51:51 +0530

    My portfolio Resume

---
+++ demo.js
@@ -0,0 +1,8 @@
+function button1(){
+    document.getElementById('nameinp').innerText=" Name : Ajay Arun Unawane";
+    document.getElementById('emailinp').innerText="Email : ajayunawane9921@gmail.com";
+    document.getElementById('ageinp').innerText="Age : 21";
+    document.getElementById('Mobileinp').innerText="Contact : 9822139070";
+    document.getElementById('placeinp').innerText="Place : Koradgaon";
+  
+  }
